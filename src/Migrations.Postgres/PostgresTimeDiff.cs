80b2ae7f544c904ba9b812e6ee3c8bb367e0a675
小羊 <tlylz99@live.cn>
2021-06-06 21:51:21 +0800

    Add initial definitions for npgsql.

---
+++ src/Migrations.Postgres/PostgresTimeDiff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SatelliteSite
+{
+    public class PostgresTimeDiff : IDurationCalculator
+    {
+        public PostgresTimeDiff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Expression<Func<DateTimeOffset, DateTimeOffset, double>> Expression { get; }
+            = (start, end) => (end - start).TotalSeconds;
+    }
+}
